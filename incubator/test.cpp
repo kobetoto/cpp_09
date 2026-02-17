baf5078a919cbdd673b96841cbddce023f0cfb8a
Thomas David <thodavid@student.42.fr>
2026-02-17 15:11:43 +0100

    RPN Zuse4 Ablesebefehl

--- incubator/test.cpp
+++ incubator/test.cpp
@@ -2,18 +2,20 @@
 #include <string>     // std::string, std::stod
 
 
-std::string new_date(const std::string &date){
-
-	std::string newDate = date;
-
-	return (newDate);
+bool    is_operator(char c){
+        
+        std::string op = "+-/*";
+        std::size_t found= op.find(c);
+        if (found!=std::string::npos)
+                return (true);
+        else
+                return (false);
 }
 
 
-
-int main ()
+int main (int ac, char **av)
 {
-  std::string date = "2011-01-04";
-  std::cout << new_date(date) << '\n';
+  (void) ac;
+  std::cout << is_operator(av[1][0]) << '\n';
   return 0;
 }
